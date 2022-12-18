76c1e360c7cd1fd90f027f66f5acff18d34f2bc5
Alexey Matveev <david.lucky.star@gmail.com>
2022-12-18 12:38:23 +0400

    Update Wordle.tsx
    function

--- src/Wordle.tsx
+++ src/Wordle.tsx
@@ -47,3 +47,118 @@ export const Wordle: FC = () => {
         </div>
     )
 }
+
+// here you go
+interface LetterGuess {
+    letter: string;
+    guess: 'correct' | 'wrong-spot' | 'wrong';
+}
+
+export const makeGuess = (answer: string, guess: string): Array<LetterGuess> => {
+    const result: Array<LetterGuess> = [];
+    for (let i = 0; i < answer.length; i++) {
+        const a = answer.charAt(i);
+        const g = guess.charAt(i);
+        if (g === a) {
+            result.push({
+                letter: g,
+                guess: 'correct',
+            });
+        } else if (answer.indexOf(g) !== -1) {
+            result.push({
+                letter: g,
+                guess: 'wrong-spot',
+            });
+        } else {
+            result.push({
+                letter: g,
+                guess: 'wrong',
+            });
+        }
+    }
+    return result;
+}
+
+import { makeGuess } from "./wordle_0";
+
+describe('Wordle test suite', () => {
+    it('Simple positive case', () => {
+        const res = makeGuess('hello', 'holls');
+        // console.log(JSON.stringify(guess1, null, 2))
+        const expected = [
+            {
+                "letter": "h",
+                "guess": "correct"
+            },
+            {
+                "letter": "o",
+                "guess": "wrong-spot"
+            },
+            {
+                "letter": "l",
+                "guess": "correct"
+            },
+            {
+                "letter": "l",
+                "guess": "correct"
+            },
+            {
+                "letter": "s",
+                "guess": "wrong"
+            }
+        ]
+        expect(res).toMatchObject(expected);
+    });
+    it('Second "l" should be in wrong spot', () => {
+        const res = makeGuess('hello', 'chill');
+        const expected = [
+            {
+                "letter": "c",
+                "guess": "wrong"
+            },
+            {
+                "letter": "h",
+                "guess": "wrong-spot"
+            },
+            {
+                "letter": "i",
+                "guess": "wrong"
+            },
+            {
+                "letter": "l",
+                "guess": "correct"
+            },
+            {
+                "letter": "l",
+                "guess": "wrong-spot"
+            }
+        ]
+        expect(res).toMatchObject(expected);
+    });
+    it('Letter "e" is duplicated twice', () => {
+        const res = makeGuess('hello', 'peace');
+        const expected = [
+            {
+                "letter": "p",
+                "guess": "wrong"
+            },
+            {
+                "letter": "e",
+                "guess": "correct"
+            },
+            {
+                "letter": "a",
+                "guess": "wrong"
+            },
+            {
+                "letter": "c",
+                "guess": "wrong"
+            },
+            {
+                "letter": "e",
+                "guess": "wrong"
+            }
+        ]
+        expect(res).toMatchObject(expected);
+    });
+});
